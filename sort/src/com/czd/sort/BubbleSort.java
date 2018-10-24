dca5c28c3ed4241f40356ff3d629e5a1ea8cf00a
chenzd <chenzd@wasu.com>
2018-10-24 16:23:57 +0800

    鸡尾酒排序（冒泡排序升级）

--- sort/src/com/czd/sort/BubbleSort.java
+++ sort/src/com/czd/sort/BubbleSort.java
@@ -13,23 +13,24 @@ public class BubbleSort {
      */
     public static void main(String[] args) {
         int[] numbers={2,36,56,42,58,69,87,89,99};
-        commonBubbleSort(numbers);
+//        commonBubbleSort(numbers);
 //        upperBubbleSort1(numbers);
 //        upperBubbleSort2(numbers);
+        cockTailSort(numbers);
     }
    /**
     * 普通的冒泡排序
     */
     private static void commonBubbleSort(int[] numbers) {
-        int n=numbers.length;
-        int  count=0;
-        for (int i=0;i<n-1;i++){
+        int n = numbers.length;
+        int count = 0;
+        for (int i=0;i<n-1;i++) {
             for (int j = 0; j <n-1-i; j++) {
                 count++;
-                if(numbers[j]>numbers[j+1]){
-                    int temp=numbers[j];
-                    numbers[j]=numbers[j+1];
-                    numbers[j+1]=temp;
+                if (numbers[j]>numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
                 }
             }
         }
@@ -45,18 +46,18 @@ public class BubbleSort {
     private static void upperBubbleSort1(int[] numbers){
         int n=numbers.length;
         int count=0;
-        for (int i=0;i<n-1;i++){
-            boolean flag=false;
+        for (int i=0;i<n-1;i++) {
+            boolean flag = false;
             for (int j = 0; j <n-1-i; j++) {
                 count++;
-                if(numbers[j]>numbers[j+1]){
-                    int temp=numbers[j];
-                    numbers[j]=numbers[j+1];
-                    numbers[j+1]=temp;
-                    flag=true;
+                if (numbers[j]>numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                    flag = true;
                 }
             }
-            if(!flag){
+            if (!flag) {
                 break;
             }
         }
@@ -70,31 +71,77 @@ public class BubbleSort {
      * 优化冒泡排序 （未发生交换，就直接停止；优化内部循环次数）
      */
     private static void upperBubbleSort2(int[] numbers){
-        int n=numbers.length;
-        int count=0;
+        int n = numbers.length;
+        int count = 0;
         //记录已经排序好的下标值，下次直接从这个下标开始比较
-        int k=n-1;
-        for (int i=0;i<n-1;i++){
-            boolean flag=false;
-            int min=0;
+        int k = n-1;
+        for (int i=0;i<n-1;i++) {
+            boolean flag = false;
+            int min = 0;
             for (int j = 0; j <k; j++) {
                 count++;
-                if(numbers[j]>numbers[j+1]){
-                    int temp=numbers[j];
-                    numbers[j]=numbers[j+1];
-                    numbers[j+1]=temp;
-                    flag=true;
-                    min=j;
+                if (numbers[j] > numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                    flag = true;
+                    min = j;
                 }
             }
-            if(!flag){
+            if (!flag) {
                 break;
             }
-            k=min;
+            k = min;
         }
         System.out.println("优化的冒泡排序2:");
         for (int i = 0; i <n ; i++) {
             System.out.println(numbers[i]);
+        }
+        System.out.println("count is "+count);
+    }
+    /**
+     * 鸡尾酒排序 （奇数轮从左到右 偶数轮从右到左 适用于基本有须的数组）
+     */
+    public static void cockTailSort(int[] numbers) {
+        int count = 0;
+        int length = numbers.length;
+        int lastRightIndex = 0;
+        int lastLeftIndex = 0;
+        int rightBorder = length-1;
+        int leftBorder = 0;
+        for (int i = 0; i <length/2; i++) {
+            boolean flag = false;
+            //奇数轮从右到左
+            for (int j = 0; j <rightBorder ; j++) {
+                count++;
+                if (numbers[j] > numbers[j+1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                    flag = true;
+                    lastRightIndex = j;
+                }
+            }
+            rightBorder = lastRightIndex;
+            if (!flag) {
+                break;
+            }
+            flag = false;
+            //偶数轮从左到右
+            for (int j = length-1-i ; j > leftBorder ; j--) {
+                count++;
+                if (numbers[j] < numbers[j-1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = temp;
+                    flag = true;
+                    lastLeftIndex = j;
+                }
+            }
+            leftBorder = lastLeftIndex;
+            if (!flag) {
+                break;
+            }
         }
         System.out.println("count is "+count);
     }
